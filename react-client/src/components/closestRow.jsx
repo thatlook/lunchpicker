e46398c9d0ac228aab6b27b60a8b30706568fb41
Yirey Suh <yrysuh@gmail.com>
2018-10-19 16:14:33 -0500

    senior present

--- react-client/src/components/closestRow.jsx
+++ react-client/src/components/closestRow.jsx
@@ -5,30 +5,52 @@ class ClosestRow extends React.Component {
     super(props);
 
     this.state = {
-      visited: []
+      visited: [],
+      changed: 0
+    }
+
+    // add cross out
+    this.tdStyle = {
+      textDecoration: 'line-through'
     }
   }
 
   handleOmit(event){
     event.preventDefault();
     // send to DB that this person went
-
+    console.log('>>> event target value', event.target.value)
+    this.state.visited.push(event.target.value)
+    // set state!!
   }
 
   render(){
     let {name, url, rating, } = this.props.shop;
-
     let i = this.props.index + 1;
-
     let linkedName = <a href={url}>{name}</a>
-    return (
-        <tr>
-          <td>{i}</td>
-          <td>{linkedName}</td>
-          <td>{rating}</td>
-          <td><button onClick={this.handleOmit.bind(this)}>Went</button></td>
-        </tr>
-    )
+
+    console.log('>>> visited', this.state.visited)
+    
+    if (this.state.visited.includes(i)) {
+      return (
+        <tr style={this.tdStyle}>
+        <td>{i}</td>
+        <td>{linkedName}</td>
+        <td>{rating}</td>
+        <td><button value={i - 1} onClick={this.handleOmit.bind(this)}>Went</button></td>
+        </tr>        
+      )
+    } else {
+      return (
+          <tr>
+            <td>{i}</td>
+            <td>{linkedName}</td>
+            <td>{rating}</td>
+            <td><button value={i - 1} onClick={this.handleOmit.bind(this)}>Went</button></td>
+          </tr>
+      )
+
+    }
+
   }
 
 }
